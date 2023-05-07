015fcbb4eb4dd960dfa11763f7132f3cdd120017
A.erfagh <ali77erfagh99@gmail.com>
2023-05-07 15:10:25 +0330

    BinarySearch algorithm done

--- src/main/java/com/javaBoy/searchAlgorithm/BinarySearch.java
+++ src/main/java/com/javaBoy/searchAlgorithm/BinarySearch.java
@@ -3,23 +3,32 @@ package com.javaBoy.searchAlgorithm;
 public class BinarySearch {
 
     private final int[] array;
-    private final int numberGuess;
+    private final int key;
+    private int findNumber = -1;
 
-    public BinarySearch(int[] array, int numberGuess) {
+    public BinarySearch(int[] array, int key) {
         this.array = array;
-        this.numberGuess = numberGuess;
-    }
-
-    public int getNumberGuess() {
-        return numberGuess;
+        this.key = key;
     }
 
     public int search() {
-        return 0;
-    }
+        int firstElement = 0;
+        int lastElement = array.length - 1;
+        int middleElement = (lastElement + firstElement) / 2;
 
-    public int[] getArray() {
-        return array;
+        while (firstElement <= lastElement) {
+            if (array[middleElement] < key)
+            {
+                firstElement = middleElement + 1;
+            } else if (array[middleElement] == key) {
+                findNumber = array[middleElement];
+                break;
+            } else {
+                lastElement = middleElement - 1;
+            }
+            middleElement = (lastElement + firstElement) / 2;
+        }
+        return findNumber;
     }
 
 
--- src/test/java/com/javaBoy/searchAlgorithm/BinarySearchTest.java
+++ src/test/java/com/javaBoy/searchAlgorithm/BinarySearchTest.java
@@ -1,7 +1,30 @@
 package com.javaBoy.searchAlgorithm;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class BinarySearchTest {
+    @Test
+    void should_find_number_key() {
 
+        int[] array = {10, 20, 30, 40, 50};
+        BinarySearch binarySearch = new BinarySearch(array, 30);
+        int result = binarySearch.search();
+
+        Assertions.assertThat(result).isEqualTo(30);
+
+
+        int[] array1 = {7, 30, 78, 213};
+        BinarySearch binarySearch1 = new BinarySearch(array1, 213);
+        int result1 = binarySearch1.search();
+
+        Assertions.assertThat(result1).isEqualTo(213);
+
+        int[] array2 = {213,321,543,6543,6754,2134432};
+        BinarySearch binarySearch2 = new BinarySearch(array2, 6754);
+        int result2 = binarySearch2.search();
+
+        Assertions.assertThat(result2).isEqualTo(6754);
+
+    }
 }
