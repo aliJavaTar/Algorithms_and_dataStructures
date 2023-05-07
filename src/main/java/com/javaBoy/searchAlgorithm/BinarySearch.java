016a33f80745036fc0f8df7053d495661cf42400
A.erfagh <ali77erfagh99@gmail.com>
2023-05-07 14:03:41 +0330

    add class BinarySearch and add test (  TDD )

--- src/main/java/com/javaBoy/searchAlgorithm/BinarySearch.java
+++ src/main/java/com/javaBoy/searchAlgorithm/BinarySearch.java
@@ -1,4 +1,26 @@
 package com.javaBoy.searchAlgorithm;
 
 public class BinarySearch {
+
+    private final int[] array;
+    private final int numberGuess;
+
+    public BinarySearch(int[] array, int numberGuess) {
+        this.array = array;
+        this.numberGuess = numberGuess;
+    }
+
+    public int getNumberGuess() {
+        return numberGuess;
+    }
+
+    public int search() {
+        return 0;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+
 }
---
+++ src/test/java/com/javaBoy/searchAlgorithm/BinarySearchTest.java
@@ -0,0 +1,7 @@
+package com.javaBoy.searchAlgorithm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTest {
+
+}
