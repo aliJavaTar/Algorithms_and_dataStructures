c90c920f556002d7f3ea56eb0bed0240aaba6bcc
aliJavaTar <ali77erfagh99@gmail.com>
2023-10-30 21:18:57 +0330

    change record to class

--- src/main/java/com/javaBoy/dataStructure/array/kata/Find.java
+++ src/main/java/com/javaBoy/dataStructure/array/kata/Find.java
@@ -1,14 +1,20 @@
 package com.javaBoy.dataStructure.array.kata;
 
-public record Find(int[] numbers) {
+public class Find {
 
-    public Find {
+    private final int[] numbers;
+
+    public Find(int[] numbers) {
         if (numbers.length < 3) throw new IllegalArgumentException("you hava to three element");
+        this.numbers=numbers;
     }
 
     public int[] findLargestThree() {
-        int firstLargest = 0, secondLargest = 0, thirdLargest = 0;
-        int firstIndex = 0, secondIndex = 0;
+        int firstLargest = 0;
+        int secondLargest = 0;
+        int thirdLargest = 0;
+        int firstIndex = 0;
+        int secondIndex = 0;
 
         for (int index = 0; index < numbers.length; index++) {
             if (firstLargest < numbers[index]) {
@@ -25,7 +31,7 @@ public record Find(int[] numbers) {
         }
 
         for (int index = 0; index < numbers.length; index++) {
-            if (thirdLargest < numbers[index] && (index != firstIndex && index !=secondIndex)) {
+            if (thirdLargest < numbers[index] && (index != firstIndex && index != secondIndex)) {
                 thirdLargest = numbers[index];
             }
 
